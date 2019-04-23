c6bdc5de79a262679b4e265115aa25eabe9741fc
falk <falk.reichbott@flam.de>
2019-04-23 18:44:53 +0200

    Merge branch 'master' of 192.168.85.201:FL5
    # Conflicts:
    #	doc/readme.doc.txt

--- tools/text2cman.c
+++ tools/text2cman.c
@@ -36,100 +36,11 @@
 static const int FALSE = 0;
 static const int TRUE = 1;
 
-int escape_file(char* inputName, FILE* inFile, FILE* outFile, int count)
-{
-        int n,i,k;
-        int startcomment;
-        int isCommentBlock = 0;
-        char linebuf[1024];
-        char pageName[2048];
-        int linecount = count;
-
-        if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
-                return 0;
-
-        linecount++;
-        n = strlen(linebuf);
-        if (n > 2 && strncmp("(1)\n", &(linebuf[n-4]),4) == 0) {
-                if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
-                        return linecount;
-
-                linecount++;
-                n = strlen(linebuf);
-                if (strncmp("===", linebuf, 3) != 0)
-                        fprintf(stderr, "WARNING unexpected start of file %s\n", inputName);
-                if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
-                        return linecount;
-
-                linecount++;
-                n = strlen(linebuf);
-        }
-        while (n <= 2) {
-                if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
-                        return linecount;
-
-                linecount++;
-                n = strlen(linebuf);
-                if (n > 2 && linebuf[0] != '\n' && linebuf[0] != '\r')
-                        break;
-        }
-        isCommentBlock=0;
-        while (1) {
-                if (n>=4 && strncmp(linebuf,"////", 4)==0) {
-                        isCommentBlock=isCommentBlock?0:1;
-                        if (isCommentBlock)
-                                startcomment = linecount;
-                }
-                if (!isCommentBlock && !(n>=2 && strncmp(linebuf,"//", 2)==0)) {
-                        pageName[0] = '"';
-                        for (i=0, k=1 ; i < n ; i++) {
-                                if (linebuf[i] == '\n') { /* escape literal newline */
-                                        pageName[k++] = '\\';
-                                        pageName[k++] = 'n';
-                                        pageName[k++] = '"';
-                                        pageName[k++] = '\n';
-                                } else if (linebuf[i] == '"') { /* escape quotation mark */
-                                        pageName[k++] = '\\';
-                                        pageName[k++] = '"';
-                                } else if (linebuf[i] == '$' && linebuf[i+1] == '{') { /* skip variables */
-                                        pageName[k++] = '"';
-                                        pageName[k++] = ' ';
-                                        for (i+=2 ; i < n ; i++) {
-                                                if (linebuf[i] == '}') {
-                                                        pageName[k++] = ' ';
-                                                        pageName[k++] = '"';
-                                                        break;
-                                                }
-                                                pageName[k++] = linebuf[i];
-                                        }
-                                } else {
-                                        pageName[k++] = linebuf[i];
-                                }
-                        }
-                        if (pageName[k-1] != '\n') { /* add newline if it is missing at end of line */
-                                pageName[k++] = '\\';
-                                pageName[k++] = 'n';
-                                pageName[k++] = '"';
-                                pageName[k++] = '\n';
-                        }
-                        pageName[k++] = 0;
-                        fputs(pageName, outFile);
-                }
-                if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
-                        break;
-                linecount++;
-                n = strlen(linebuf);
-        }
-        if (isCommentBlock) {
-                fprintf(stderr, "%s:%d:1: Error: Unclosed comment block. Comment blocks must be closed by \"////\" on the start of a line!\n", inputName, startcomment);
-        }
-        return linecount;
-}
 
 int main(int argc, char* argv[])
 {
         char  c;
-        int   n, i, j;
+        int   n, i, j, k;
         int   singleFiles = TRUE;
         FILE* inFile;
         FILE* outFile = NULL;
@@ -140,7 +51,8 @@ int main(int argc, char* argv[])
         char pageName[2048];
         char stringName[512];
         char* prefix = "";
-        int linecount = 0;
+        int isCommentBlock = 0;
+        int linecount = 0, startcomment;
 
         while ((n = getopt(argc, argv, "o:p:q")) != -1) {
                 switch (n) {
@@ -207,11 +119,86 @@ int main(int argc, char* argv[])
                         }
                         stringName[i++] = toupper(c);
                         if (c == '/' || c == '\\') /* skip directory name */
-                                i = 0;
+                            i = 0;
                 }
                 sprintf(pageName, "static const char %s%s[] =\n", prefix, stringName);
                 fputs(pageName, outFile);
-                linecount = escape_file(inputName, inFile, outFile, linecount);
+
+                if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
+                        goto thisistheend;
+
+                linecount++;
+                n = strlen(linebuf);
+                if (n > 2 && strncmp("(1)\n", &(linebuf[n-4]),4) == 0) {
+                    if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
+                        goto thisistheend;
+
+                    linecount++;
+                    n = strlen(linebuf);
+                    if (strncmp("===", linebuf, 3) != 0)
+                        fprintf(stderr, "WARNING unexpected start of file %s\n", inputName);
+                    if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
+                        goto thisistheend;
+
+                    linecount++;
+                    n = strlen(linebuf);
+                }
+                while (n <= 2) {
+                    if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
+                        goto thisistheend;
+
+                    linecount++;
+                    n = strlen(linebuf);
+                    if (n > 2 && linebuf[0] != '\n' && linebuf[0] != '\r')
+                        break;
+                }
+                isCommentBlock=0;
+                while (1) {
+                        if (n>=4 && strncmp(linebuf,"////", 4)==0) {
+                           isCommentBlock=isCommentBlock?0:1;
+                           if (isCommentBlock)
+                              startcomment = linecount;
+                        }
+                        if (!isCommentBlock && !(n>=2 && strncmp(linebuf,"//", 2)==0)) {
+                           pageName[0] = '"';
+                           for (i=0, k=1 ; i < n ; i++) {
+                                   if (linebuf[i] == '\n') { /* escape literal newline */
+                                      pageName[k++] = '\\';
+                                      pageName[k++] = 'n';
+                                      pageName[k++] = '"';
+                                      pageName[k++] = '\n';
+                                   } else if (linebuf[i] == '"') { /* escape quotation mark */
+                                      pageName[k++] = '\\';
+                                      pageName[k++] = '"';
+                                   } else if (linebuf[i] == '$' && linebuf[i+1] == '{') { /* skip variables */
+                                       pageName[k++] = '"';
+                                       pageName[k++] = ' ';
+                                       for (i+=2 ; i < n ; i++) {
+                                           if (linebuf[i] == '}') {
+                                               pageName[k++] = ' ';
+                                               pageName[k++] = '"';
+                                               break;
+                                           }
+                                           pageName[k++] = linebuf[i];
+                                       }
+                                   } else {
+                                      pageName[k++] = linebuf[i];
+                                   }
+                           }
+                           if (pageName[k-1] != '\n') { /* add newline if it is missing at end of line */
+                              pageName[k++] = '\\';
+                              pageName[k++] = 'n';
+                              pageName[k++] = '"';
+                              pageName[k++] = '\n';
+                           }
+                           pageName[k++] = 0;
+                           fputs(pageName, outFile);
+                        }
+                        if (NULL == fgets(linebuf, sizeof(linebuf), inFile))
+                                break;
+                        linecount++;
+                        n = strlen(linebuf);
+                }
                 fclose(inFile);
                 strcpy(pageName, "#ifdef __SHOW_TEXT_SOURCE\n");
                 fputs(pageName, outFile);
@@ -224,8 +211,17 @@ int main(int argc, char* argv[])
                         fclose(outFile);
                         outFile = NULL;
                 }
+                if (isCommentBlock) {
+                   fprintf(stderr, "%s:%d:1: Error: Unclosed comment block. Comment blocks must be closed by \"////\" on the start of a line!\n", inputName, startcomment);
+                   return -1;
+                }
         }
         if (!singleFiles)
-                fclose(outFile);
+            fclose(outFile);
         return 0;
+thisistheend:
+        fclose(inFile);
+        if (NULL != outFile)
+                fclose(outFile);
+        return -1;
 }
